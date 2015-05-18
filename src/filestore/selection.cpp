e77a34a87716aed7e00c7c16594bbc1cf19f5875
Brendan Whitfield <brendanw@windworksdesign.com>
2015-05-17 23:41:54 -0400

    added GPL notices to all source files

--- src/filestore/selection.cpp
+++ src/filestore/selection.cpp
@@ -1,4 +1,33 @@
 
+/*
+########################################################################
+#                                                                      #
+# Collector: Tag based file management for Linux                       #
+#                                                                      #
+# Copyright 2015 Brendan Whitfield (bcw7044@rit.edu)                   #
+#                                                                      #
+########################################################################
+#                                                                      #
+# selection.cpp                                                        #
+#                                                                      #
+# This file is part of Collector                                       #
+#                                                                      #
+# Collector is free software: you can redistribute it and/or modify    #
+# it under the terms of the GNU General Public License as published by #
+# the Free Software Foundation, either version 2 of the License, or    #
+# (at your option) any later version.                                  #
+#                                                                      #
+# Collector is distributed in the hope that it will be useful,         #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
+# GNU General Public License for more details.                         #
+#                                                                      #
+# You should have received a copy of the GNU General Public License    #
+# along with Collector.  If not, see <http://www.gnu.org/licenses/>.   #
+#                                                                      #
+########################################################################
+*/
+
 
 #include <string>
 #include <iostream>
